8971c816b7ed30e400b56b6ea775c34259c06c2e
minju <minju9711@naver.com>
2025-03-23 22:54:05 +0900

    [add] 레디스 게임 핸들러 클래스 추가

---
+++ src/main/java/spring/socket_server/pubsub/GamePubSubHandler.java
@@ -0,0 +1,42 @@
+package spring.socket_server.pubsub;
+
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+import spring.socket_server.dto.GameReadyStatus;
+
+import static spring.socket_server.config.WebSocketConstants.*;
+
+
+@Service
+public class GamePubSubHandler extends PubSubHandler {
+
+    public GamePubSubHandler(SimpMessagingTemplate messagingTemplate) {
+        super(messagingTemplate);
+    }
+
+    @Override
+    protected void initHandlers() {
+        handlers.put(GAME_READY_CHANNEL, this::handleGameReady);
+        handlers.put(GAME_START_CHANNEL, this::handleGameStart);
+        handlers.put(GAME_END_CHANNEL, this::handleGameEnd);
+        handlers.put(GAME_INFO_CHANNEL, this::handleGameInfo);
+    }
+
+    private void handleGameReady(String channel, String message) {
+        GameReadyStatus gameReadyStatus = convertMessageToDto(message, GameReadyStatus.class);
+        messagingTemplate.convertAndSend(channel, gameReadyStatus);
+    }
+
+    private void handleGameStart(String channel, String message) {
+        messagingTemplate.convertAndSend(channel, message);
+    }
+
+    private void handleGameEnd(String channel, String message) {
+        messagingTemplate.convertAndSend(channel, message);
+    }
+
+    private void handleGameInfo(String channel, String message) {
+
+    }
+
+}
