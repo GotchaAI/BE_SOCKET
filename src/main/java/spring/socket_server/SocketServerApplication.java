3e618b3e5db7c8952563fb600e8b1b8bcaf5bb75
minju <minju9711@naver.com>
2025-03-23 20:23:12 +0900

    [add] 첫 커밋

---
+++ src/main/java/spring/socket_server/SocketServerApplication.java
@@ -0,0 +1,13 @@
+package spring.socket_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocketServerApplication.class, args);
+    }
+
+}
