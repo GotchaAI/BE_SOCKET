5ff40becbee637b30c0399dce23ccb59d678b6b2
minju <minju9711@naver.com>
2025-03-23 22:52:37 +0900

    [docs] 앞으로의 엔드포인트 todo 정리

--- src/main/java/spring/socket_server/controller/WebSocketController.java
+++ src/main/java/spring/socket_server/controller/WebSocketController.java
@@ -10,6 +10,7 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Controller;
 import spring.socket_server.Manager.ChannelManager;
 import spring.socket_server.dto.ChatMessageReq;
+import spring.socket_server.dto.GameEventReq;
 import spring.socket_server.dto.GameReadyStatus;
 
 import static spring.socket_server.config.WebSocketConstants.*;
@@ -60,6 +61,27 @@ public class WebSocketController {
     @MessageMapping("/game/ready/{roomId}")
     public void sendReadyStatus(@DestinationVariable String roomId, @Payload GameReadyStatus readyStatus) {
         pubSubHandler.convertAndSend(GAME_READY_CHANNEL   + roomId, readyStatus);
+    }
+
+    //todo : 5. 게임 시작 전 게임 정보(제시어) 전달
+    @MessageMapping("/game/info/{roomId}")
+    public void sendGameInfoBeforeStart(@DestinationVariable String roomId, String roundCount) { //라운드 수 같이 줘야 함.
+    }
+
+    // todo : 6. 게임 시작 ( 레디 상태가 최소 인원 넘는지 확인,
+    @MessageMapping("/game/start/{roomId}")
+    public void startGame(@DestinationVariable String roomId, @Payload String nickName, @Header("simpSessionId") String sessionId) {
+
+    }
+
+    // todo : 7. 게임 종료
+    @MessageMapping("/game/end/{roomId}")
+    public void endGame(@DestinationVariable String roomId, @Payload String nickName,  @Header("simpSessionId") String sessionId) {
+    }
+
+    //todo : 8. 대기방 퇴장
+    @MessageMapping("/game/exit/{roomId}")
+    public void exitGameRoom(@DestinationVariable String roomId, @Header("simpSessionId") String sessionId) {
     }
 
 }
