c7f60c72e65e91cdd76b715b127fae869ed497e5
minju <minju9711@naver.com>
2025-05-05 05:32:30 +0900

    [add] 커스텀 예외 처리 클래스 추가

---
+++ src/main/java/spring/socket_server/common/exception/global/CustomException.java
@@ -0,0 +1,11 @@
+package spring.socket_server.common.exception.global;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import spring.socket_server.common.exception.ExceptionCode;
+
+@Getter
+@AllArgsConstructor
+public class CustomException extends RuntimeException{
+    private final ExceptionCode exceptionCode;
+}
