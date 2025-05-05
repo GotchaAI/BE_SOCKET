8a6f9d07148fdb852bcf9d3af4440723d834a8d0
minju <minju9711@naver.com>
2025-05-05 17:26:08 +0900

    [modify] 예외code 형식 http 서버와 동일하게 변경

--- src/main/java/spring/socket_server/common/exception/room/RoomExceptionCode.java
+++ src/main/java/spring/socket_server/common/exception/room/RoomExceptionCode.java
@@ -7,16 +7,16 @@ import spring.socket_server.common.exception.ExceptionCode;
 @AllArgsConstructor
 public enum RoomExceptionCode implements ExceptionCode {
     USER_ALREADY_IN_ANOTHER_ROOM(
-            HttpStatus.BAD_REQUEST, "ROOM_001", "이미 다른 방에 입장 중 입니다."
+            HttpStatus.BAD_REQUEST, "ROOM_400_001", "이미 다른 방에 입장 중 입니다."
     ),
     USER_ALREADY_IN_ROOM(
-            HttpStatus.BAD_REQUEST, "ROOM_002", "이미 이 방에 존재 합니다."
+            HttpStatus.BAD_REQUEST, "ROOM_400_002", "이미 이 방에 존재 합니다."
     ),
     USER_NOT_IN_ROOM(
-            HttpStatus.BAD_REQUEST, "ROOM_003", "방에 존재하지 않는 유저입니다."
+            HttpStatus.BAD_REQUEST, "ROOM_400__003", "방에 존재하지 않는 유저입니다."
     ),
     NOT_ROOM_OWNER(
-            HttpStatus.FORBIDDEN, "ROOM_004", "방장 권한이 없습니다."
+            HttpStatus.FORBIDDEN, "ROOM_403_001", "방장 권한이 없습니다."
     );
 
     private final HttpStatus status;
