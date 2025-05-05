21e50d2ddc038cddfdb790c5f40d1ef58f0267f1
minju <minju9711@naver.com>
2025-05-05 17:21:47 +0900

    [add] 현재 http 서버 gloablException 코드 추가 [add] 유효하지 않은 채널 시 Exception 코드 추가 [add] 유효하지 않은 메시지 dto 형태 Exception 코드 추가

--- src/main/java/spring/socket_server/common/exception/global/GlobalExceptionCode.java
+++ src/main/java/spring/socket_server/common/exception/global/GlobalExceptionCode.java
@@ -6,7 +6,12 @@ import spring.socket_server.common.exception.ExceptionCode;
 
 @AllArgsConstructor
 public enum GlobalExceptionCode implements ExceptionCode {
-INVALID_FIELD(HttpStatus.BAD_REQUEST, "GLOBAL_001", "지원하지 않는 필드입니다.");
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "GLOBAL-500-001", "서버 에러입니다. 서버 팀에 문의해주세요."),
+    FIELD_VALIDATION_ERROR(HttpStatus.BAD_REQUEST, "GLOBAL-400-001", "요청한 필드 값이 유효하지 않습니다."),
+    CSRF_INVALID(HttpStatus.FORBIDDEN, "GLOBAL-403-001", "CSRF 토큰이 유효하지 않습니다."),
+    USER_NOT_FOUND(HttpStatus.UNAUTHORIZED, "AUTH-401-001", "인증된 사용자를 찾을 수 없습니다."),
+    INVALID_MESSAGE_FORMAT(HttpStatus.BAD_REQUEST, "GLOBAL_400_002", "유효하지 않은 메시지 포맷 입니다."),
+    INVALID_CHANNEL(HttpStatus.BAD_REQUEST, "GLOBAL_400_003", "유효하지 않은 채널입니다.");
 
     private final HttpStatus status;
     private final String code;
