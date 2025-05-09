193bc7e24370179166209587d4ab4be19820bb84
minju <minju9711@naver.com>
2025-05-10 05:50:43 +0900

    add: 방 고유 코드 생성 로직 추가. modify : map의 key에 고유 prefix 부여

--- src/main/java/spring/socket_server/domain/room/service/RoomService.java
+++ src/main/java/spring/socket_server/domain/room/service/RoomService.java
@@ -3,6 +3,7 @@ package spring.socket_server.domain.room.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import spring.socket_server.domain.room.RoomField.RoomField;
 import spring.socket_server.domain.room.dto.CreateRoomRequest;
 import spring.socket_server.domain.room.model.RoomMetadata;
@@ -12,32 +13,37 @@ import java.util.Map;
 @Service
 @RequiredArgsConstructor
 public class RoomService {
+    private final RoomIdService roomIdService;
     private final RedisTemplate<String, String> redisTemplate;
 
-    public void createRoom(CreateRoomRequest request, String ownerId) {
-        String key = getRoomKey(request.roomId());
+    //todo : lua 스크립트 적용
+    public String createRoom(CreateRoomRequest request, String ownerId) {
+        String roomId = roomIdService.allocateRoomId();
 
-        Map<String, String> roomData = Map.of(
+        Map<String, String> roomData = Map.of (
                 RoomField.TITLE.getRedisField(), request.title(),
                 RoomField.OWNER.getRedisField(), ownerId,
                 RoomField.HAS_PASSWORD.getRedisField(), String.valueOf(request.hasPassword()),
                 RoomField.PASSWORD.getRedisField(), request.password(),
                 RoomField.MAX.getRedisField(), String.valueOf(request.gameMode().getMaxPlayers()),
                 RoomField.MIN.getRedisField(), String.valueOf(request.gameMode().getMinPlayers()),
-                RoomField.AI_LEVEL.getRedisField(), request.Aimode().name(),
+                RoomField.AI_LEVEL.getRedisField(), request.aimode().name(),
                 RoomField.GAME_MODE.getRedisField(), request.gameMode().name()
         );
 
-        redisTemplate.opsForHash().putAll(key, roomData);
+        redisTemplate.opsForHash().putAll(getRoomKey(roomId), roomData);
+        return roomId;
     }
 
+    //todo : lua 스크립트 적용
     public void deleteRoom (String roomId) {
         redisTemplate.delete(getRoomKey(roomId));
         redisTemplate.delete(getRoomKey(roomId) + ":users");
         redisTemplate.delete(getRoomKey(roomId) + ":chatlog");
+        roomIdService.releaseRoomId(roomId);
     }
 
-    public RoomMetadata updateRoomField(String roomId, String fieldName, String value) {
+    public RoomMetadata updateRoomField (String roomId, String fieldName, String value) {
         RoomField field = RoomField.from(fieldName);
         redisTemplate.opsForHash().put(getRoomKey(roomId), field.getRedisField(), value);
         return getRoomInfo(roomId);
@@ -45,7 +51,7 @@ public class RoomService {
 
     public RoomMetadata getRoomInfo(String roomId) {
         Map<Object, Object> fields = redisTemplate.opsForHash().entries(getRoomKey(roomId));
-        return RoomMetadata.fromRedisMap(fields);
+        return RoomMetadata.fromRedisMap(roomId, fields);
     }
 
     private String getRoomKey(String roomId) {
