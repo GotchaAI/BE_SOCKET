db479a6b00b636973bdf4b1dc940f61735d67ce0
minju <minju9711@naver.com>
2025-05-10 05:20:51 +0900

    [refactor] 웹소켓 @Valid를 통한 에러 처리 불가능으로 인해 CreateRoomRequest의 validation 어노테이션 제거

--- src/main/java/spring/socket_server/domain/room/dto/CreateRoomRequest.java
+++ src/main/java/spring/socket_server/domain/room/dto/CreateRoomRequest.java
@@ -1,14 +1,12 @@
 package spring.socket_server.domain.room.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import spring.socket_server.domain.game.enumType.AiMode;
 import spring.socket_server.domain.game.enumType.GameMode;
 
 public record CreateRoomRequest(
-        @NotBlank String roomId,
-        @NotBlank String title,
+        String title,
         boolean hasPassword,
         String password,
-        @NotBlank AiMode Aimode,
-        @NotBlank GameMode gameMode
+        AiMode aimode,
+        GameMode gameMode
 ) {}
