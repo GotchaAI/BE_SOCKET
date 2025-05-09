d42ce8ad31b640b9bce9e346fc16011b247fa951
minju <minju9711@naver.com>
2025-05-10 05:49:25 +0900

    remove: redis의 key로 쓰일 roomid 삭제

--- src/main/java/spring/socket_server/domain/room/dto/UpdateRoomFieldRequest.java
+++ src/main/java/spring/socket_server/domain/room/dto/UpdateRoomFieldRequest.java
@@ -3,7 +3,6 @@ package spring.socket_server.domain.room.dto;
 import jakarta.validation.constraints.NotBlank;
 
 public record UpdateRoomFieldRequest(
-        @NotBlank String roomId,
         @NotBlank String field,
         @NotBlank String value
 ) {}
