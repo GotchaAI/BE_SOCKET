2088077403fe144347ae889d64eef66e1a040d27
minju <minju9711@naver.com>
2025-05-10 05:49:53 +0900

    add:  MethodArgumentNotValidException 처리 함수 추가

--- src/main/java/spring/socket_server/common/exception/SocketGlobalExceptionHandler.java
+++ src/main/java/spring/socket_server/common/exception/SocketGlobalExceptionHandler.java
@@ -2,11 +2,16 @@ package spring.socket_server.common.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import spring.socket_server.common.exception.global.CustomException;
 import spring.socket_server.common.exception.global.FieldValidationException;
 import spring.socket_server.common.exception.global.GlobalExceptionCode;
+import org.springframework.validation.FieldError;
+
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Slf4j
 @ControllerAdvice
@@ -19,14 +24,25 @@ public class SocketGlobalExceptionHandler {
         return ExceptionRes.from(error);
     }
 
-    @MessageExceptionHandler(IllegalArgumentException.class)
+    @MessageExceptionHandler(FieldValidationException.class)
     @SendToUser("/queue/errors")
     public ExceptionRes handleInvalidField(final FieldValidationException e) {
-        e.getFieldErrors().forEach((field, message) ->
-                log.warn("[Field Validation Exception] {}: {}", field, message)
-        );
         ExceptionCode error = GlobalExceptionCode.FIELD_VALIDATION_ERROR;
         return ExceptionRes.from(error, e.getFieldErrors());
     }
 
+    @MessageExceptionHandler(MethodArgumentNotValidException.class)
+    @SendToUser("/queue/errors")
+    public ExceptionRes handleValidationException(final MethodArgumentNotValidException e) {
+        Map<String, String> fieldErrors = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .collect(Collectors.toMap (
+                        FieldError::getField,
+                        FieldError::getDefaultMessage,
+                        (existing, replacement) -> existing
+                ));
+        return ExceptionRes.from(GlobalExceptionCode.FIELD_VALIDATION_ERROR, fieldErrors);
+    }
 }
+
