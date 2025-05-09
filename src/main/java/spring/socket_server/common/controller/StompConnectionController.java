6944e9b6367ab423f67a5dfd2a5ffa2028be4c29
minju <minju9711@naver.com>
2025-05-10 05:46:16 +0900

    chore : WebSocket 첫 연결 시 호출 함수. 추후 사용 계획 주석 처리

--- src/main/java/spring/socket_server/common/controller/StompConnectionController.java
+++ src/main/java/spring/socket_server/common/controller/StompConnectionController.java
@@ -6,18 +6,16 @@ import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Controller;
-import spring.socket_server.common.manager.ChannelManager;
 
 //WebSocket으로 들어온 메시지를 Redis에 발행
 @Controller
 @RequiredArgsConstructor
 public class StompConnectionController {
-    private final ChannelManager channelManager;
 
     // WebSocket 첫 연결 시 초기 채널 관리
     @MessageMapping("/connect")
     public void onConnect(@Payload String nickName, @Header("simpSessionId") String sessionId) {
-        channelManager.subscribeToInitialChannels(nickName, sessionId);
+        //추후 세션ID ↔ 유저ID 매핑에 사용. 지금은 사용 x
     }
 
 }
